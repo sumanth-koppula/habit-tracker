65e952b75fd3f0cd11807571489c8c9949ea6dc5
sumanth-koppula <58146547+sumanth-koppula@users.noreply.github.com>
2023-06-23 13:45:33 +0530

    Update db.js

--- app/db.js
+++ app/db.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 exports.connectMongoose =()=>{
     mongoose.set("strictQuery", false);
-    mongoose.connect('mongodb://127.0.0.1:27017/habit-tracter-development', { useNewUrlParser: true })
+    mongoose.connect('mongodb+srv://sumanth:12345@cluster0.atmmqfe.mongodb.net/cluster0?retryWrites=true&w=majority', { useNewUrlParser: true })
     .then((e)=>console.log("Connected to Mongodb => Habit-Tracker"))
     .catch((e)=>console.log("Not Connect Mongodb", e));
 }
